8ad378a4e63824ba4a58422afc19bf2e00ad3f7f
Atish Patel <atishpatel2012@gmail.com>
2016-01-16 18:04:21 -0600

    updated auth file

--- auth/auth.go
+++ auth/auth.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/appengine"
 
 	"github.com/atishpatel/Gigamunch-Backend/config"
+	"github.com/atishpatel/Gigamunch-Backend/databases/session"
 	"github.com/atishpatel/Gigamunch-Backend/types"
 	"github.com/atishpatel/Gigamunch-Backend/utils"
 )
@@ -20,49 +21,70 @@ const (
 )
 
 var (
-	gitkitClient *gitkit.Client
-	serverConfig *config.Config
+	gitkitClient    *gitkit.Client
+	gitkitAudiences []string
 )
 
-// GetUserFromSessionID returns a User object if user session is found
+// GetUserFromSessionID returns channel that returns types.User if user session is found
 // otherwise, it return nil
-func GetUserFromSessionID(sessionID string) *types.User {
+func GetUserFromSessionID(ctx context.Context, sessionID string) <-chan *types.User {
 	//TODO fix with redis stuff, check if sessionID is valid
-	return nil
+	return session.GetUserSession(ctx, sessionID)
 }
 
-func GetUserAndSessionFromGToken(ctx context.Context, tokenString string) (*types.User, string) {
+// GetUserAndSessionFromGToken returns a user with a valid sessionID if the gtoken is valid
+// otherwise, it returns nil, ""
+func GetUserAndSessionFromGToken(ctx context.Context, tokenString string) (string, *types.User) {
 	if tokenString == "" {
-		return nil, ""
+		return "", nil
 	}
-	token, err := gitkitClient.ValidateToken(ctx, tokenString, []string{serverConfig.ClientID})
+	token, err := gitkitClient.ValidateToken(ctx, tokenString, gitkitAudiences)
 	if err != nil {
 		utils.Errorf(ctx, "Invalid gitkit token %s: %+v", tokenString, err)
-		return nil, ""
+		return "", nil
 	}
 	if time.Now().Sub(token.IssueAt) > 15*time.Minute {
 		utils.Infof(ctx, "Token %s is too old. Issued at: %s", tokenString, token.IssueAt)
-		return nil, ""
+		return "", nil
 	}
 	// get user info from gitkit servers
-	//u, err := gitkitClient.UserByLocalID(ctx, token.LocalID)
+	gitkitUser, err := gitkitClient.UserByLocalID(ctx, token.LocalID)
 	if err != nil {
 		utils.Errorf(ctx, "Failed to fetch user info from gitkit servers %s(%s): %+v", token.Email, token.LocalID, err)
-		return nil, ""
+		return "", nil
 	}
 
-	// TODO(Atish): check if can use getmulti on redis
+	sessionID, user := createSession(ctx, gitkitUser)
+
+	return sessionID, user
+}
+
+func createSession(ctx context.Context, gitkitUser *gitkit.User) (string, *types.User) {
+	// only set name like that if it's the first time the user in logging in
+	user := &types.User{
+		Email: gitkitUser.Email,
+	}
+
+	// TODO(Atish): fix setting permissions for session
 	// chefChan := getBasicChefInfo(u.Email)
 	// muncherChan := getBasicMuncherInfo(u.Email)
 
-	sessionID := CreateSession(ctx)
+	// TODO(Atish): If first time logging in, download PhotoURL to server
+	//
 
-	return nil, sessionID
+	sessionID := utils.GetUUID()
+	errChan := SaveUserSession(ctx, sessionID, user)
+	err := <-errChan
+	if err != nil {
+		utils.Errorf(ctx, "Failed to save user session", err)
+		return "", nil
+	}
+	return sessionID, user
 }
 
-func createSession(ctx context.Context) string {
-	//TODO: Probably takes in a user and returns a sessionID
-	return utils.GetUUID()
+// SaveUserSession saves the tpyes.User value with the sessionID key
+func SaveUserSession(ctx context.Context, sessionID string, user *types.User) <-chan error {
+	return session.SaveUserSession(ctx, sessionID, user)
 }
 
 // CurrentUser extracts the user information stored in current session.
@@ -80,19 +102,19 @@ func CurrentUser(req *http.Request, w http.ResponseWriter) *types.User {
 		if ts == "" {
 			return nil
 		}
-		user, sessionID := getUserFromGToken(ctx, ts)
-		// TODO fix
-		utils.Infof(ctx, "tmp to help golang build", sessionID)
+		_, user := GetUserAndSessionFromGToken(ctx, ts)
+		// TODO Set session cookie and remove gtoken
 		return user
 	} else if err != nil {
 		utils.Errorf(ctx, "Error getting user: %+v", err)
 	}
 	sessionID := sessionCookie.Value
-	return GetUserFromSessionID(sessionID)
+	userChan := GetUserFromSessionID(ctx, sessionID)
+	return <-userChan
 }
 
 func init() {
-	serverConfig = config.GetConfig()
+	serverConfig := config.GetConfig()
 	// setup gitkit
 	c := &gitkit.Config{
 		WidgetURL: types.GitkitURL,
@@ -105,11 +127,5 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// databaseConfig := databases.Config{
-	// 	RedisSessionDBIP:       config.RedisSessionServerIP,
-	// 	RedisSessionDBPassword: config.RedisSessionServerPassword
-	//	RedisSessionDBPoolSize: 10}
-	// db := databases.CreateDatabase(databaseConfig)
-
+	gitkitAudiences = []string{serverConfig.ClientID}
 }
