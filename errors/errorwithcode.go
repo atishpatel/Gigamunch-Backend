7bfb891e87c5dcdea0aa9d0049f0be4bf69e4f4b
Atish Patel <atishpatel2012@gmail.com>
2016-04-15 15:12:32 -0500

    removed unnessary err field and fixed details bug and added comments

--- errors/errorwithcode.go
+++ errors/errorwithcode.go
@@ -6,19 +6,26 @@ type ErrorWithCode struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 	Details string `json:"details"`
-	err     error  // err is private because of endpoints
 }
 
 func (err ErrorWithCode) Error() string {
-	return fmt.Sprintf("Errorcode %d: %s: %+v", err.Code, err.Message, err.err)
+	return fmt.Sprintf("Errorcode %d: %s: %+v", err.Code, err.Message, err.Details)
 }
 
-func (err ErrorWithCode) WithError(attachedErr error) ErrorWithCode {
-	err.err = attachedErr
-	err.Details = err.Error()
+// WithMessage sets Message. Makes chaining easier.
+func (err ErrorWithCode) WithMessage(msg string) ErrorWithCode {
+	err.Message = msg
 	return err
 }
 
+// WithError sets Details to error.Error()
+func (err ErrorWithCode) WithError(e error) ErrorWithCode {
+	err.Details = e.Error()
+	return err
+}
+
+// GetErrorWithCode casts error as ErrorWithCode or creates an ErrorWithCode
+// with CodeUnkownError as the code
 func GetErrorWithCode(err error) ErrorWithCode {
 	if errWithCode, ok := err.(ErrorWithCode); ok {
 		return errWithCode
