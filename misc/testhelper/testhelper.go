1f8d4aa9cc4c6e9ee91ecc81b1d7433c51176489
Atish Patel <atishpatel2012@gmail.com>
2016-05-07 11:17:14 -0500

    forgot to add testhelper for test helping

---
+++ misc/testhelper/testhelper.go
@@ -0,0 +1,52 @@
+package testhelper
+
+import (
+	"github.com/atishpatel/Gigamunch-Backend/core/gigamuncher"
+	"github.com/atishpatel/Gigamunch-Backend/types"
+	"golang.org/x/net/context"
+)
+
+const (
+	FoodPhotoURL   = "http://lorempixel.com/1920/1080/food"
+	PersonPhotoURL = "http://lorempixel.com/1920/1080/people"
+)
+
+func GetGigamuncher(ctx context.Context) (string, gigamuncher.Gigamuncher) {
+	// TODO create datastore if not exist
+	return "gigamuncher", gigamuncher.Gigamuncher{
+		UserDetail: types.UserDetail{
+			Name:     "Muncher Name",
+			Email:    "muncher@test.com",
+			PhotoURL: FoodPhotoURL,
+		},
+	}
+}
+
+func GetGigamuncherAddress() types.Address {
+	return getAddress()
+}
+
+func GetGigachefUser() types.User {
+	return types.User{
+		ID:          "gigachef",
+		Name:        "Chef Name",
+		Email:       "chef@test.com",
+		ProviderID:  "google.com",
+		PhotoURL:    PersonPhotoURL,
+		Permissions: 2147483647,
+	}
+}
+
+func getAddress() types.Address {
+	// Nashville Parthenon
+	return types.Address{
+		Street:  "2500 West End Avenue",
+		City:    "Nashville",
+		State:   "TN",
+		Country: "USA",
+		GeoPoint: types.GeoPoint{
+			Latitude:  36.149535,
+			Longitude: -86.813154,
+		},
+	}
+}
