0dfa7c6195518e3261ecba08c86b635912ac4e6f
Atish Patel <atishpatel2012@gmail.com>
2016-05-20 13:54:34 -0500

    updated startserver script

--- startservers.sh
+++ startservers.sh
@@ -3,28 +3,31 @@
 ################################################################################
 # start redis
 ################################################################################
-echo 'starting redis'
-redis-server > /dev/null &
+# echo 'starting redis'
+# redis-server > ./redislog.tmp &
 ################################################################################
 # setup mysql
 ################################################################################
 /usr/local/opt/mysql56/bin/mysql.server start
 # create gigamunch database
-cat misc/create_gigamunch_datbase.sql | mysql -uroot
-# create live_posts table
-mysql -uroot gigamunch < misc/create_live_posts_table.sql
-# TODO create user for get and create mysql and one of cron delete
+cat misc/setup.sql | mysql -uroot
 ################################################################################
 # goapp
 ################################################################################
 echo 'starting goapp'
-dev_appserver.py --datastore_path ./.datastore endpoint-gigachef/app.yaml server/app-dev.yaml # endpoint-gigamuncher/app.yaml
+if [ $1 == "muncher" ]; then
+  echo "using muncher and server"
+  dev_appserver.py --datastore_path ./.datastore endpoint-gigamuncher/app.yaml server/app-dev.yaml
+else
+  echo "using chef and server"
+  dev_appserver.py --datastore_path ./.datastore endpoint-gigachef/app.yaml server/app-dev.yaml
+fi
 ################################################################################
 # clean up
 ################################################################################
-echo 'stopping mysql'
+# stop mysql
 /usr/local/opt/mysql56/bin/mysql.server stop
 
-echo 'stopping redis server'
-redis-cli shutdown
+# echo 'stopping redis server'
+# redis-cli shutdown
 wait
