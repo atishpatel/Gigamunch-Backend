363be151d4a8efac0d6eb7745b05e2ff6ca14c22
Atish Patel <atishpatel2012@gmail.com>
2016-06-09 13:52:11 -0500

    changed get functions to GET

--- endpoint-gigachef/service.go
+++ endpoint-gigachef/service.go
@@ -26,12 +26,40 @@ func ttos(t time.Time) string {
 	return strconv.FormatInt(t.Unix(), 10)
 }
 
+func stot(ts string) (time.Time, error) {
+	ti, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return time.Now(), err
+	}
+	return time.Unix(ti, 0), nil
+}
+
 func itos(i int64) string {
 	return strconv.FormatInt(i, 10)
 }
 
+func ftos(f float64) string {
+	return strconv.FormatFloat(f, 'f', 2, 32)
+}
+
 func stoi(s string) (int64, error) {
 	return strconv.ParseInt(s, 10, 64)
+}
+
+func stoi32(s string) (int32, error) {
+	i, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return int32(i), err
+}
+
+func stof(s string) (float32, error) {
+	f, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		return 0, err
+	}
+	return float32(f), nil
 }
 
 type coder interface {
@@ -76,7 +104,6 @@ func init() {
 		i := m.Info()
 		i.Name, i.HTTPMethod, i.Path, i.Desc = name, method, path, desc
 	}
-	// TODO update to GET
 	// refresh stuff
 	register("RefreshToken", "refreshToken", "POST", "gigachefservice/refreshToken", "Refresh a token.")
 	// application stuff
@@ -86,9 +113,11 @@ func init() {
 	register("GetSubMerchant", "getSubMerchant", "GET", "gigachefservice/getSubMerchant", "Get the sub merchant info.")
 	// item stuff
 	register("SaveItem", "saveItem", "POST", "gigachefservice/saveItem", "Save a item.")
-	register("GetItem", "getItem", "POST", "gigachefservice/getItem", "Get a item.")
-	register("GetItems", "getItems", "POST", "gigachefservice/getItems", "Gets items sorted by last used.")
+	register("GetItem", "getItem", "GET", "gigachefservice/getItem", "Get a item.")
+	register("GetItems", "getItems", "GET", "gigachefservice/getItems", "Gets items sorted by last used.")
 	// post stuff
 	register("PostPost", "postPost", "POST", "gigachefservice/postPost", "Post a post.")
+	register("GetPosts", "getPosts", "GET", "gigachefservice/getPosts", "Get a chef's posts.")
+
 	endpoints.HandleHTTP()
 }
