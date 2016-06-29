186c716657b0fb9dcc47a3f65e49f1639d4e96d3
Atish Patel <atishpatel2012@gmail.com>
2016-06-28 19:05:42 -0500

    muncher addresses endpoints

--- core/gigamuncher/account.go
+++ core/gigamuncher/account.go
@@ -3,6 +3,8 @@ package gigamuncher
 import (
 	"time"
 
+	"gitlab.com/atishpatel/Gigamunch-Backend/core/maps"
+
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
 
@@ -13,6 +15,16 @@ import (
 var (
 	errDatastore = errors.ErrorWithCode{Code: errors.CodeInternalServerErr, Message: "Error with datastore."}
 )
+
+// Client is a client for gigamuncher
+type Client struct {
+	ctx context.Context
+}
+
+// New returns a client for gigamuncher
+func New(ctx context.Context) *Client {
+	return &Client{ctx: ctx}
+}
 
 // SaveUserInfo is to save a user's info. Only exposed for account package.
 // Please use the account package's func instead of this one.
@@ -90,4 +102,55 @@ func SaveBTCustomerID(ctx context.Context, muncherID, btCustomerID string) error
 		}
 	}
 	return nil
+}
+
+// GetAddresses returns the addresses for a gigamuncher
+func (c *Client) GetAddresses(muncherID string) ([]Addresses, error) {
+	muncher := new(Gigamuncher)
+	err := get(c.ctx, muncherID, muncher)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrap("cannot get gigamuncher")
+	}
+	return muncher.Addresses, nil
+}
+
+// SelectAddress adds and selects the address as main address
+func (c *Client) SelectAddress(muncherID string, address *types.Address) (*Addresses, error) {
+	muncher := new(Gigamuncher)
+	err := get(c.ctx, muncherID, muncher)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrap("cannot get gigamuncher")
+	}
+	var found bool
+	var a *Addresses
+	for i := range muncher.Addresses {
+		// check if address already exists
+		if address.String() == muncher.Addresses[i].Address.String() {
+			muncher.Addresses[i].Selected = true
+			a = &muncher.Addresses[i]
+			found = true
+		} else {
+			muncher.Addresses[i].Selected = false
+		}
+	}
+	// add address if not found
+	if !found {
+		if address.Longitude == 0 && address.Latitude == 0 {
+			err = maps.GetGeopointFromAddress(c.ctx, address)
+			if err != nil {
+				return nil, errors.Wrap("failed to maps.GetGeopointFromAddress", err)
+			}
+		}
+		a = &Addresses{
+			Address:       *address,
+			AddedDateTime: time.Now(),
+			Selected:      true,
+		}
+		muncher.Addresses = append(muncher.Addresses, *a)
+	}
+	err = put(c.ctx, muncherID, muncher)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrap("cannot put gigamuncher")
+	}
+	return a, nil
 }
