f80270adb46bc1a7c4cc8d92cf0e95bbcf18d3ff
Atish Patel <atishpatel2012@gmail.com>
2016-07-08 13:49:45 -0500

    updated to exchange time segments

--- core/post/post.go
+++ core/post/post.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/atishpatel/Gigamunch-Backend/core/notification"
+	"gitlab.com/atishpatel/Gigamunch-Backend/core/item"
+	"gitlab.com/atishpatel/Gigamunch-Backend/core/notification"
 
 	"appengine"
 
@@ -47,64 +48,148 @@ type Resp struct {
 	Post
 }
 
-// PostPost posts a live post if the post is valid
-// returns postID, error
-func PostPost(ctx context.Context, user *types.User, post *Post) (int64, error) {
+type notifyClient interface {
+	SendSMS(string, string) error
+}
+
+type chefClient interface {
+	GetPostInfo(string) (*gigachef.PostInfoResp, error)
+	IncrementNumPost(string) error
+}
+
+type itemClient interface {
+	IncrementNumPostsCreated(int64) error
+	AddNumTotalOrders(int64, int32) error
+}
+
+// PublishPostReq is the req to publish a post
+type PublishPostReq struct {
+	User                      *types.User    `json:"user"`
+	BaseItem                  types.BaseItem `json:"base_item"`
+	ItemID                    int64          `json:"item_id"`
+	Title                     string         `json:"title"`
+	ClosingDateTime           time.Time      `json:"closing_datetime"`
+	StartPickupDateTime       time.Time      `json:"start_pickup_datetime"`
+	EndPickupDateTime         time.Time      `json:"end_pickup_datetime"`
+	ChefDelivery              bool           `json:"chef_delivery"`
+	ChefDeliveryRadius        int32          `json:"chef_delivery_radius"`
+	ChefDeliveryBasePrice     float32        `json:"chef_delivery_base_price"`
+	StartChefDeliveryDateTime time.Time      `json:"start_chef_delivery_datetime"`
+	EndChefDeliveryDateTime   time.Time      `json:"end_chef_delivery_datetime"`
+	ServingsOffered           int32          `json:"servings_offered"`
+	ChefPricePerServing       float32        `json:"chef_price_per_serving"`
+}
+
+// PublishPost publishes a post into the food feed
+func (c *Client) PublishPost(req *PublishPostReq) (int64, *Post, error) {
+	nC := notification.New(c.ctx)
+	chefC := gigachef.New(c.ctx)
+	itemC := item.New(c.ctx)
+	return publishPost(c.ctx, req, chefC, itemC, nC)
+}
+
+func publishPost(ctx context.Context, req *PublishPostReq, chefC chefClient, itemC itemClient, nC notifyClient) (int64, *Post, error) {
 	var err error
-	if !user.IsVerifiedChef() {
-		return 0, errNotVerifiedChef
+	if !req.User.IsVerifiedChef() {
+		return 0, nil, errNotVerifiedChef
 	}
-	post.CreatedDateTime = time.Now().UTC()
-	post.TaxPercentage = taxPercentage
-	post.GigachefID = user.ID
-	post.PricePerServing = post.ChefPricePerServing * 1.2
-	if post.IsOrderNow {
-		post.ReadyDateTime = post.ClosingDateTime
-	}
-	// get the gigachef post info
-	postInfo, err := gigachef.GetPostInfo(ctx, user)
+	// get the chef post info
+	postInfo, err := chefC.GetPostInfo(req.User.ID)
 	if err != nil {
-		return 0, err
+		return 0, nil, errors.Wrap("failed to chef.GetPostInfo", err)
 	}
 	if !postInfo.Address.Valid() {
-		return 0, errUnauthorized.WithMessage("User does not have an address.")
+		return 0, nil, errUnauthorized.WithMessage("User does not have an address.")
 	}
 	if postInfo.BTSubMerchantStatus == "" {
-		return 0, errNoSubMerchantID
+		return 0, nil, errNoSubMerchantID
 	}
-	post.GigachefAddress = postInfo.Address
-	post.GigachefDelivery.Radius = postInfo.DeliveryRange
-	post.BTSubMerchantID = postInfo.BTSubMerchantID
-	post.GigachefDelivery.MaxDuration = 45 * 60 // 45 minutes
-	// IncrementNumPost for gigachef
-	postErrChan := make(chan error, 1)
-	go func() {
-		postErrChan <- gigachef.IncrementNumPost(ctx, user)
-	}()
-	// TODO add IncrementNumCreated post for Item
+	// create post
+	post := &Post{
+		BaseItem:        req.BaseItem,
+		ItemID:          req.ItemID,
+		Title:           req.Title,
+		ClosingDateTime: req.ClosingDateTime,
+		GigachefDelivery: GigachefDelivery{
+			Radius:    req.ChefDeliveryRadius,
+			BasePrice: req.ChefDeliveryBasePrice,
+		},
+		ServingsOffered:     req.ServingsOffered,
+		ChefPricePerServing: req.ChefPricePerServing,
+		TaxPercentage:       taxPercentage,
+		PricePerServing:     req.ChefPricePerServing * 1.2,
+		GigachefAddress:     postInfo.Address,
+		BTSubMerchantID:     postInfo.BTSubMerchantID,
+	}
+	post.CreatedDateTime = time.Now()
+	post.GigachefID = req.User.ID
+	// add pickup options
+	const pickupBuffer = 30 * time.Minute
+	var pickupExchange types.ExchangeMethods
+	pickupExchange.SetPickup(true)
+	pickupTime := req.StartPickupDateTime
+	endTime := req.EndPickupDateTime.Add(-pickupBuffer + (1 * time.Minute))
+	for pickupTime.Before(endTime) {
+		post.ExchangeTimes = append(post.ExchangeTimes, ExchangeTimeSegment{
+			StartDateTime:            pickupTime,
+			EndDateTime:              pickupTime.Add(pickupBuffer),
+			AvailableExchangeMethods: pickupExchange,
+		})
+		pickupTime = pickupTime.Add(pickupBuffer)
+	}
+	// add chef delivery options
+	if req.ChefDelivery {
+		const chefDeliveryBuffer = 1 * time.Hour
+		var chefDeliveryExchange types.ExchangeMethods
+		endTime := req.EndChefDeliveryDateTime.Add(-chefDeliveryBuffer + (1 * time.Minute))
+		chefDeliveryExchange.SetChefDelivery(true)
+		chefDeliveryTime := req.StartChefDeliveryDateTime
+		for chefDeliveryTime.Before(endTime) {
+			post.ExchangeTimes = append(post.ExchangeTimes, ExchangeTimeSegment{
+				StartDateTime:            chefDeliveryTime,
+				EndDateTime:              chefDeliveryTime.Add(chefDeliveryBuffer),
+				AvailableExchangeMethods: chefDeliveryExchange,
+			})
+			chefDeliveryTime = chefDeliveryTime.Add(chefDeliveryBuffer)
+		}
+	}
+	// make post
 	// put in datastore
 	postID, err := putIncomplete(ctx, post)
 	// insert into sql table
 	err = insertLivePost(ctx, postID, post)
 	if err != nil {
-		// TODO update to a transaction
 		utils.Criticalf(ctx, "failed to put %d in sql database: +%v", postID, err)
-		return 0, errMySQL.WithError(err).Wrap("mysql insert failed")
+		return 0, nil, errMySQL.WithError(err).Wrap("mysql insert failed")
 	}
-	<-postErrChan
+	// IncrementNumPost for gigachef and posts
+	const numRoutines = 2
+	errChan := make(chan error, numRoutines)
+	go func() {
+		errChan <- chefC.IncrementNumPost(req.User.ID)
+	}()
+	go func() {
+		errChan <- itemC.IncrementNumPostsCreated(post.ItemID)
+	}()
 	if !appengine.IsDevAppServer() {
 		// notify enis
-		nC := notification.New(ctx)
 		var photo string
 		if len(post.Photos) != 0 {
 			photo = post.Photos[0]
 		}
-		err = nC.SendSMS("6153975516", fmt.Sprintf("A new post was made by %s. \n Title: %s \n Desc: %s \n Image: %s", user.Name, post.Title, post.Description, photo))
+		err = nC.SendSMS("6153975516", fmt.Sprintf("A new post was made. \n Title: %s \n Desc: %s \n Image: %s \n\nPostID: %s", post.Title, post.Description, photo, postID))
 		if err != nil {
-			utils.Criticalf(ctx, "failed to notify enis about chef(%s) making a post(%s)", user.ID, postID)
+			utils.Criticalf(ctx, "failed to notify enis about chef(%s) making a post(%s)", req.User.ID, postID)
 		}
 	}
-	return postID, nil
+	// flush goroutines
+	for i := 0; i < numRoutines; i++ {
+		err = <-errChan
+		if err != nil {
+			utils.Errorf(ctx, "error while publishingPost: %s", err)
+		}
+	}
+	return postID, post, nil
 }
 
 // GetUserPosts gets post from a user sorted by ready time
