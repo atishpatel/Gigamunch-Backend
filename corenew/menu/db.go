53233d8f24f508e383664f0ef656825eafe69360
Atish Patel <atishpatel2012@gmail.com>
2016-09-15 17:25:12 -0500

    added get functions to menu

--- corenew/menu/db.go
+++ corenew/menu/db.go
@@ -6,16 +6,33 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
-func get(ctx context.Context, id string) (*Menu, error) {
+func get(ctx context.Context, id int64) (*Menu, error) {
 	menu := new(Menu)
-	key := datastore.NewKey(ctx, kindMenu, id, 0, nil)
+	key := datastore.NewKey(ctx, kindMenu, "", id, nil)
 	err := datastore.Get(ctx, key, menu)
 	return menu, err
 }
 
-func put(ctx context.Context, id string, menu *Menu) error {
+// getCookMenus returns a list of Menus
+func getCookMenus(ctx context.Context, cookID string) ([]int64, []Menu, error) {
+	query := datastore.NewQuery(kindMenu).
+		Filter("CookID =", cookID).
+		Limit(1000)
+	var results []Menu
+	keys, err := query.GetAll(ctx, &results)
+	if err != nil {
+		return nil, nil, err
+	}
+	ids := make([]int64, len(keys))
+	for i := range keys {
+		ids[i] = keys[i].IntID()
+	}
+	return ids, results, nil
+}
+
+func put(ctx context.Context, id int64, menu *Menu) error {
 	var err error
-	key := datastore.NewKey(ctx, kindMenu, id, 0, nil)
+	key := datastore.NewKey(ctx, kindMenu, "", id, nil)
 	_, err = datastore.Put(ctx, key, menu)
 	return err
 }
--- corenew/menu/types.go
+++ corenew/menu/types.go
@@ -1,6 +1,9 @@
 package menu
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const kindMenu = "Menu"
 
@@ -10,5 +13,37 @@ type Menu struct {
 	EditedDateTime  time.Time `json:"edited_datetime" datastore:",noindex"`
 	CookID          string    `json:"cook_id" datastore:",index"`
 	Name            string    `json:"name" datastore:",noindex"`
-	Color           string    `json:"color" datastore:",noindex"`
+	Color           Color     `json:"color" datastore:",noindex"`
+}
+
+type Color int32
+
+// HexValue returns the hex value of the color
+func (c Color) HexValue() string {
+	val := int32(c)
+	switch val {
+	case 0: // green
+		return "#4CAF50"
+	case 1: // teal
+		return "#009688"
+	case 2: // cyan
+		return "#00BCD4"
+	}
+	return "#4CAF50"
+}
+
+func (c Color) MarshalJSON() ([]byte, error) {
+	v := fmt.Sprintf("\"%s\"", c.HexValue())
+	return []byte(v), nil
+}
+
+func (c *Color) UnmarshalJSON(data []byte) error {
+	colorString := string(data[1 : len(data)-1])
+	switch colorString {
+	case "teal":
+		*c = 1
+	case "cyan":
+		*c = 2
+	}
+	return nil
 }
