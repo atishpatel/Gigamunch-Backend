1e6e21f3aa92d80aedf767802878d0f983c09f35
Atish Patel <atishpatel2012@gmail.com>
2016-10-10 15:54:39 -0500

    added build command to bash script to build proto files

--- app.sh
+++ app.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
 ################################################################################
-# start redis
+# build 
 ################################################################################
-# echo 'starting redis'
-# redis-server > ./redislog.tmp &
-################################################################################
-# setup mysql
-################################################################################
-/usr/local/opt/mysql56/bin/mysql.server start
-# create gigamunch database
-cat misc/setup.sql | mysql -uroot
-################################################################################
-# goapp
-################################################################################
-if [[ $1 == "eater" ]]; then
-  echo "starting eaterapi and server"
-  dev_appserver.py --datastore_path ./.datastore endpoint-gigamuncher/app.yaml server/app-dev.yaml
-elif [[ $1 == "server" ]]; then
-  echo "starting server"
-  dev_appserver.py --datastore_path ./.datastore server/app-dev.yaml
-else
-  echo "starting cookapi and server"
-  dev_appserver.py --datastore_path ./.datastore cookapi/app.yaml server/app-dev.yaml
+if [[ $1 == "build" ]]; then
+  # build protobuf and grpc
+  echo "Building Gigamunch-Proto eater api."
+  protoc -I Gigamunch-Proto/common/ -I Gigamunch-Proto/eater/ Gigamunch-Proto/common/*.proto Gigamunch-Proto/eater/*.proto --go_out=plugins=grpc:Gigamunch-Proto/eater
+  exit 0
 fi
-################################################################################
-# clean up
-################################################################################
-# stop mysql
-/usr/local/opt/mysql56/bin/mysql.server stop
 
-# echo 'stopping redis server'
-# redis-cli shutdown
+
+################################################################################
+# serve
+################################################################################
+if [[ $1 == "serve" ]] || [[ $1 == "" ]]; then
+  # setup mysql
+  /usr/local/opt/mysql56/bin/mysql.server start
+  # create gigamunch database
+  cat misc/setup.sql | mysql -uroot
+  # start goapp serve
+  if [[ $2 == "eater" ]]; then
+    echo "Starting eaterapi and server."
+    dev_appserver.py --datastore_path ./.datastore endpoint-gigamuncher/app.yaml server/app-dev.yaml
+  else
+    echo "Starting cookapi and server."
+    dev_appserver.py --datastore_path ./.datastore cookapi/app.yaml server/app-dev.yaml
+  fi
+  # stop mysql
+  /usr/local/opt/mysql56/bin/mysql.server stop
+fi 
+
 wait
