f83b68bdd827a46a8acb0fd2c2016ca78f49e176
Atish Patel <atishpatel2012@gmail.com>
2016-10-10 15:51:47 -0500

    added new sql tables

--- misc/setup.sql
+++ misc/setup.sql
@@ -1,25 +1,28 @@
 -- create database
-CREATE DATABASE IF NOT EXISTS gigamunch;
+CREATE DATABASE IF NOT EXISTS gigamunch CHARACTER SET utf8mb4;
 -- use database
 USE gigamunch;
 -- create live_posts table
-CREATE TABLE IF NOT EXISTS `live_posts` (
- `post_id` BIGINT NOT NULL PRIMARY KEY,
- `item_id` BIGINT NOT NULL,
- `gigachef_id` VARCHAR(45) NOT NULL,
- `close_datetime` DATETIME NOT NULL, -- used for cron job
- `first_exchange_datetime` DATETIME NOT NULL,
- `last_exchange_datetime` DATETIME NOT NULL,
- `search_tags` VARCHAR( 500 ) NOT NULL,
- `is_baked_good` BOOLEAN NOT NULL DEFAULT 0,
- `latitude` FLOAT( 10, 6 ) NOT NULL,
- `longitude` FLOAT( 10, 6 ) NOT NULL,
-  FULLTEXT(`search_tags`),
+CREATE TABLE IF NOT EXISTS `active_items` (
+  `id` BIGINT NOT NULL PRIMARY KEY,
+  `menu_id` BIGINT NOT NULL,
+  `cook_id` VARCHAR(45) NOT NULL,
+  `created_datetime` DATETIME NOT NULL,
+  `cook_price_per_serving` FLOAT(10, 2) NOT NULL,
+  `min_servings` TINYINT UNSIGNED NOT NULL,
+  `max_servings` SMALLINT UNSIGNED NOT NULL,
+  `latitude` FLOAT( 10, 6 ) NOT NULL,
+  `longitude` FLOAT( 10, 6 ) NOT NULL,
+  `vegan` BOOLEAN NOT NULL DEFAULT 0,
+  `vegetarian` BOOLEAN NOT NULL DEFAULT 0, 
+  `paleo` BOOLEAN NOT NULL DEFAULT 0, 
+  `gluten_free` BOOLEAN NOT NULL DEFAULT 0, 
+  `kosher` BOOLEAN NOT NULL DEFAULT 0,
   INDEX(`latitude`),
   INDEX(`longitude`),
-  INDEX(`first_exchange_datetime`),
-  INDEX(`last_exchange_datetime`)
-) ENGINE = MYISAM ;
+  INDEX(`created_datetime`),
+  INDEX(`menu_id`)
+) ENGINE = MYISAM;
 -- create like table
 CREATE TABLE IF NOT EXISTS `like` (
   `user_id` VARCHAR(45) NOT NULL,
@@ -31,4 +34,38 @@ CREATE TABLE IF NOT EXISTS `like` (
 CREATE TABLE IF NOT EXISTS `num_likes` (
   `item_id` BIGINT NOT NULL PRIMARY KEY,
   `num` BIGINT NOT NULL
+) ENGINE = InnoDB;
+-- create review table
+CREATE TABLE IF NOT EXISTS `review` (
+  `id` BIGINT NOT NULL PRIMARY KEY,
+  `cook_id` VARCHAR(45) NOT NULL,
+  `eater_id` VARCHAR(45) NOT NULL,
+  `eater_name` VARCHAR(100) NOT NULL,
+  `eater_photo_url` VARCHAR(200) NOT NULL,
+  `inquiry_id` BIGINT NOT NULL,
+  `item_id` BIGINT NOT NULL,
+  `menu_id` BIGINT NOT NULL,
+  `created_datetime` DATETIME NOT NULL,
+  `rating` INT NOT NULL,
+  `text` VARCHAR(1200) NOT NULL,
+  `edited_datetime` DATETIME NOT NULL,
+  `is_edited` BOOLEAN NOT NULL DEFAULT 0,
+  -- cook response stuff
+  `has_response` BOOLEAN NOT NULL DEFAULT 0,
+  `response_created_datetime` DATETIME NOT NULL,
+  `response_text` VARCHAR(1200) NOT NULL,
+  INDEX(`item_id`),
+  INDEX(`created_datetime`)
+) ENGINE = InnoDB;
+-- create completed_inquries table 
+CREATE TABLE IF NOT EXISTS `completed_inquries` (
+  `id` BIGINT NOT NULL PRIMARY KEY,
+  `servings` SMALLINT UNSIGNED NOT NULL,
+  `cook_id` VARCHAR(45) NOT NULL,
+  `item_id` BIGINT NOT NULL,
+  `menu_id` BIGINT NOT NULL,
+  `created_datetime` DATETIME NOT NULL,
+  `cook_price_per_serving` FLOAT(10,2) NOT NULL,
+  `cook_revenue` FLOAT(10,2) NOT NULL,
+  INDEX(`cook_id`)
 ) ENGINE = InnoDB;
