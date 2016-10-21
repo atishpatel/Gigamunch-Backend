c5aa43a4f688add1271e2a4d986aadc3429b9b55
Atish Patel <atishpatel2012@gmail.com>
2016-10-20 20:15:31 -0500

    corenew: added review package

---
+++ corenew/review/review.go
@@ -0,0 +1,223 @@
+package review
+
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	// driver for mysql
+	mysql "github.com/go-sql-driver/mysql"
+
+	"github.com/atishpatel/Gigamunch-Backend/config"
+	"github.com/atishpatel/Gigamunch-Backend/errors"
+	"github.com/atishpatel/Gigamunch-Backend/types"
+	"github.com/atishpatel/Gigamunch-Backend/utils"
+	"golang.org/x/net/context"
+
+	"google.golang.org/appengine"
+)
+
+const (
+	// datetimeFormat        = "2006-01-02 15:04:05" //"Jan 2, 2006 at 3:04pm (MST)"
+	insertStatement       = "INSERT INTO review (cook_id,eater_id,eater_name,eater_photo_url,inquiry_id,item_id,menu_id,rating,text) VALUES ('%s','%s','%s','%s',%d,%d,%d,%d,'%s')"
+	updateStatement       = "UPDATE TABLE review (eater_name,eater_photo_url,rating,text,edited_datetime,is_edited) VALUES ('%s','%s',%d,'%s',NOW(),1) WHERE id=%d"
+	updateCookResponse    = "UPDATE TABLE review (has_response,response_created_datetime,response_text) VALUES (1,NOW(),%s) WHERE id=%d"
+	selectReviewStatement = "SELECT (id,cook_id,eater_id,eater_name,eater_photo_url,inquiry_id,item_id,menu_id,created_datetime,rating,text,is_edited,edited_datetime,has_response,response_created_datetime,response_text) FROM review WHERE id=%d %s"
+	// selectCookReviews     = "SELECT "
+	// selectByUserID   = "SELECT item_id FROM `like` WHERE user_id=? ORDER BY item_id ASC"
+)
+
+var (
+	connectOnce         = sync.Once{}
+	mysqlDB             *sql.DB
+	errSQLDB            = errors.ErrorWithCode{Code: errors.CodeInternalServerErr, Message: "Error with cloud sql database."}
+	errBuffer           = errors.ErrorWithCode{Code: errors.CodeInternalServerErr, Message: "An unknown error occured."}
+	errInvalidParameter = errors.ErrorWithCode{Code: errors.CodeInvalidParameter, Message: "Invalid parameter."}
+)
+
+// Client is the client for likes
+type Client struct {
+	ctx context.Context
+}
+
+// New returns a new Client for user likes
+func New(ctx context.Context) *Client {
+	connectOnce.Do(func() {
+		connectSQL(ctx)
+	})
+	return &Client{ctx: ctx}
+}
+
+// Post posts or updates a review.
+func (c *Client) Post(user *types.User, id int64, cookID string, inquiryID, itemID, menuID int64, rating int32, text string) (*Review, error) {
+	if rating < 1 || rating > 5 {
+		return nil, errInvalidParameter.WithMessage("A rating has to be between 1 star and 5 stars.")
+	}
+	isNewReview := id == 0
+	now := time.Now()
+	review := &Review{
+		CreatedDateTime: now,
+		EditedDateTime:  now,
+		ID:              id,
+		CookID:          cookID,
+		InquiryID:       inquiryID,
+		ItemID:          itemID,
+		MenuID:          menuID,
+		Rating:          rating,
+		Text:            text,
+	}
+	if isNewReview {
+		// insert review
+		st := fmt.Sprintf(insertStatement, cookID, user.ID, user.Name, user.PhotoURL, inquiryID, itemID, menuID, rating, text)
+		results, err := mysqlDB.Exec(st)
+		if err != nil {
+			return nil, errSQLDB.WithError(err).Wrapf("failed execute %s", st)
+		}
+		id, err = results.LastInsertId()
+		if err != nil {
+			return nil, errSQLDB.WithError(err).Wrap("failed to results.LastInsertId()")
+		}
+		review.ID = id
+	} else {
+		// update review
+		st := fmt.Sprintf(updateStatement, user.Name, user.PhotoURL, rating, text, id)
+		results, err := mysqlDB.Exec(st)
+		if err != nil {
+			return nil, errSQLDB.WithError(err).Wrapf("failed execute %s", st)
+		}
+		rowsEffected, err := results.RowsAffected()
+		if err != nil {
+			return nil, errSQLDB.WithError(err).Wrap("failed to results.RowsAffected()")
+		}
+		if rowsEffected == 0 {
+			return nil, errInvalidParameter.WithMessage("Invalid ReviewID.")
+		}
+		review.IsEdited = true
+		review.EditedDateTime = time.Now()
+	}
+	return review, nil
+}
+
+// PostResponse updates a review with the cook's response.
+func (c *Client) PostResponse(user *types.User, id int64, text string) error {
+	if id == 0 {
+		return errInvalidParameter.WithMessage("ReviewID cannot be 0.")
+	}
+	// update review
+	st := fmt.Sprintf(updateCookResponse, text, id)
+	results, err := mysqlDB.Exec(st)
+	if err != nil {
+		return errSQLDB.WithError(err).Wrapf("failed execute %s", st)
+	}
+	rowsEffected, err := results.RowsAffected()
+	if err != nil {
+		return errSQLDB.WithError(err).Wrap("failed to results.RowsAffected()")
+	}
+	if rowsEffected == 0 {
+		return errInvalidParameter.WithMessage("Invalid ReviewID.")
+	}
+	return nil
+}
+
+// func (c *Client) GetMultiByCookID() ([]*Review, error) {}
+
+func getSelectReviewStatement(ids []int64) (string, error) {
+	if len(ids) == 0 {
+		return "", errInvalidParameter
+	}
+	var err error
+	var buffer bytes.Buffer
+	for _, v := range ids[1:] {
+		_, err = buffer.WriteString(fmt.Sprintf(" OR id=%d", v))
+		if err != nil {
+			return "", errBuffer.WithError(err)
+		}
+	}
+	st := fmt.Sprintf(selectReviewStatement, ids[0], buffer.String())
+	return st, nil
+}
+
+// GetMultiByID gets multiple reviews by their ids. Reviews in the array might be nil if they are not found.
+func (c *Client) GetMultiByID(ids []int64) ([]*Review, error) {
+	reviews := make([]*Review, len(ids))
+	if len(ids) != 0 {
+		st, err := getSelectReviewStatement(ids)
+		if err != nil {
+			return nil, err
+		}
+		rows, err := mysqlDB.Query(st)
+		if err != nil {
+			return nil, errSQLDB.WithError(err).Wrapf("failed to execute %s", st)
+		}
+		defer handleCloser(c.ctx, rows)
+
+		for rows.Next() {
+			review := new(Review)
+			var createdNulltime mysql.NullTime
+			var editedNulltime mysql.NullTime
+			var responseCreatedNulltime mysql.NullTime
+			err = rows.Scan(&review.ID, &review.CookID, &review.EaterID, &review.EaterName, &review.EaterPhotoURL,
+				&review.InquiryID, &review.ItemID, &review.MenuID, &createdNulltime,
+				&review.Rating, &review.Text, &review.IsEdited, &editedNulltime,
+				&review.HasResponse, &responseCreatedNulltime, &review.ResponseText)
+			if err != nil {
+				return nil, errSQLDB.WithError(err).Wrap("cannot scan rows")
+			}
+			if createdNulltime.Valid {
+				review.CreatedDateTime = createdNulltime.Time
+			}
+			if editedNulltime.Valid {
+				review.EditedDateTime = editedNulltime.Time
+			}
+			if responseCreatedNulltime.Valid {
+				review.ResponseCreatedDateTime = responseCreatedNulltime.Time
+			}
+			for i := range ids {
+				if ids[i] == review.ID {
+					reviews[i] = review
+					break
+				}
+			}
+		}
+	}
+	return reviews, nil
+}
+
+// Get gets a review.
+func (c *Client) Get(id int64) (*Review, error) {
+	reviews, err := c.GetMultiByID([]int64{id})
+	if err != nil {
+		return nil, err
+	}
+	return reviews[0], err
+}
+
+func connectSQL(ctx context.Context) {
+	var err error
+	var connectionString string
+	if appengine.IsDevAppServer() {
+		// "user:password@/dbname"
+		connectionString = "root@/gigamunch"
+	} else {
+		projectID := config.GetProjectID(ctx)
+		connectionString = fmt.Sprintf("root@cloudsql(%s:us-central1:gigasqldb)/gigamunch", projectID)
+	}
+	mysqlDB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal("Couldn't connect to mysql database")
+	}
+}
+
+type closer interface {
+	Close() error
+}
+
+func handleCloser(ctx context.Context, c closer) {
+	err := c.Close()
+	if err != nil {
+		utils.Errorf(ctx, "Error closing rows: %v", err)
+	}
+}
---
+++ corenew/review/types.go
@@ -0,0 +1,26 @@
+package review
+
+import "time"
+
+// Review is a review from an Eater about an item. It also contains a
+// response from the Cook
+type Review struct {
+	ID              int64     `json:"id,string"`
+	CookID          string    `json:"cook_id"`
+	EaterID         string    `json:"eater_id"`
+	EaterName       string    `json:"eater_name"`
+	EaterPhotoURL   string    `json:"eater_photo_url"`
+	InquiryID       int64     `json:"inquiry_id,string"`
+	ItemID          int64     `json:"item_id,string"`
+	MenuID          int64     `json:"menu_id,string"`
+	CreatedDateTime time.Time `json:"created_datetime"`
+	// eater review
+	Rating         int32     `json:"rating"`
+	Text           string    `json:"text"`
+	IsEdited       bool      `json:"is_edited"`
+	EditedDateTime time.Time `json:"edited_datetime"`
+	// cook response
+	HasResponse             bool      `json:"has_response"`
+	ResponseCreatedDateTime time.Time `json:"response_created_datetime"`
+	ResponseText            string    `json:"response_text"`
+}
