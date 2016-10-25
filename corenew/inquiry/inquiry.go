9990f6c9a618d2340bce3d41ba0950b001e9dd0c
Atish Patel <atishpatel2012@gmail.com>
2016-10-25 15:54:00 -0500

    corenew: init inquiry

---
+++ corenew/inquiry/inquiry.go
@@ -0,0 +1,329 @@
+package inquiry
+
+import (
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/atishpatel/Gigamunch-Backend/corenew/cook"
+	"github.com/atishpatel/Gigamunch-Backend/corenew/eater"
+	"github.com/atishpatel/Gigamunch-Backend/corenew/item"
+	"github.com/atishpatel/Gigamunch-Backend/corenew/payment"
+	"github.com/atishpatel/Gigamunch-Backend/errors"
+	"github.com/atishpatel/Gigamunch-Backend/types"
+	"github.com/atishpatel/Gigamunch-Backend/utils"
+)
+
+var (
+	errDatastore        = errors.ErrorWithCode{Code: errors.CodeInternalServerErr, Message: "Error with datastore."}
+	errInvalidParameter = errors.ErrorWithCode{Code: errors.CodeInvalidParameter, Message: "Invalid parameter."}
+	errUnauthorized     = errors.ErrorWithCode{Code: errors.CodeUnauthorizedAccess, Message: "User does not have access."}
+)
+
+// Client is a client for orders
+type Client struct {
+	ctx context.Context
+}
+
+// New returns a new Client
+func New(ctx context.Context) *Client {
+	return &Client{ctx: ctx}
+}
+
+// Get gets the inquiry by ID.
+func (c *Client) Get(id int64) (*Inquiry, error) {
+	inquiry, err := get(c.ctx, id)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get inquiry(%d)", id)
+	}
+	return inquiry, nil
+}
+
+// GetByCookID gets inquiries by CookID.
+func (c *Client) GetByCookID(cookID string, startLimit, endLimit int) ([]*Inquiry, error) {
+	inquiries, err := getCookInquirys(c.ctx, cookID, startLimit, endLimit)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get inquiries with cookID(%s)", cookID)
+	}
+	return inquiries, nil
+}
+
+// GetByEaterID gets inquiries by EaterID.
+func (c *Client) GetByEaterID(eaterID string, startLimit, endLimit int) ([]*Inquiry, error) {
+	inquiries, err := getEaterInquirys(c.ctx, eaterID, startLimit, endLimit)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get inquiries with eaterID(%s)", eaterID)
+	}
+	return inquiries, nil
+}
+
+// GetByTransactionID gets the inquiry by TransactionID.
+func (c *Client) GetByTransactionID(transactionID string) (*Inquiry, error) {
+	inquiry, err := getByBTTransactionID(c.ctx, transactionID)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get inquiry with transactionID(%s)", transactionID)
+	}
+	return inquiry, nil
+}
+
+func validateMakeParams(itemID int64, nonce string, eaterID string, eaterAddress *types.Address, numServings int32) error {
+	if itemID == 0 {
+		return errInvalidParameter.WithMessage("ItemID cannot be 0.")
+	}
+	if nonce == "" {
+		return errInvalidParameter.WithMessage("Nonce cannot be empty.")
+	}
+	if eaterID == "" {
+		return errInvalidParameter.WithMessage("EaterID cannot be empty.")
+	}
+	if eaterAddress == nil {
+		return errInvalidParameter.WithMessage("EaterAddress cannot be empty.")
+	}
+	if numServings == 0 {
+		return errInvalidParameter.WithMessage("NumServings cannot be empty.")
+	}
+	return nil
+}
+
+// Make makes a new Inquiry.
+func (c *Client) Make(itemID int64, nonce string, eaterID string, eaterAddress *types.Address, numServings int32, exchangeMethod types.ExchangeMethods, expectedExchangeTime time.Time, exchangePrice float32) (*Inquiry, error) {
+	if err := validateMakeParams(itemID, nonce, eaterID, eaterAddress, numServings); err != nil {
+		return nil, err
+	}
+	// get item
+	itemC := getItemClient(c.ctx)
+	item, err := itemC.Get(itemID)
+	if err != nil {
+		return nil, errors.Wrap("failed to itemC.Get", err)
+	}
+	// get eater
+	eaterC := getEaterClient(c.ctx)
+	eater, err := eaterC.Get(eaterID)
+	if err != nil {
+		return nil, errors.Wrap("failed to eaterC.Get", err)
+	}
+	// get cook
+	cookC := getCookClient(c.ctx)
+	cook, err := cookC.Get(item.CookID)
+	if err != nil {
+		return nil, errors.Wrap("failed to cookC.Get", err)
+	}
+	// calculate pricing
+	pricePerServing := payment.GetPricePerServing(item.CookPricePerServing)
+	totalPricePerServing := pricePerServing * float32(numServings)
+	taxPercentage := payment.GetTaxPercentage(cook.Address.Latitude, cook.Address.Longitude)
+	totalPrice := (totalPricePerServing + exchangePrice) * (taxPercentage / 100)
+	totalTaxPrice := totalPrice - (totalPricePerServing + exchangePrice)
+	totalCookPrice := item.CookPricePerServing * float32(numServings)
+	totalGigaFee := totalPricePerServing - totalCookPrice
+	cookPriceWithDelivery := totalCookPrice
+	gigaFeeWithDelivery := totalGigaFee
+	if exchangeMethod.Pickup() || exchangeMethod.ChefDelivery() {
+		cookPriceWithDelivery += exchangePrice
+	} else {
+		gigaFeeWithDelivery += exchangePrice
+	}
+	// start payment
+	paymentC := getPaymentClient(c.ctx)
+	transactionID, err := paymentC.StartSale(cook.BTSubMerchantID, nonce, cookPriceWithDelivery, gigaFeeWithDelivery)
+	if err != nil {
+		return nil, errors.Wrap("failed to paymentC.StartSale", err)
+	}
+	var itemPhoto string
+	if len(item.Photos) > 0 {
+		itemPhoto = item.Photos[0]
+	}
+	// get cheap estimate for distance and duration
+	distance := eaterAddress.GreatCircleDistance(cook.Address.GeoPoint)
+	duration := eaterAddress.EstimatedDuration(cook.Address.GeoPoint)
+	inquiry := &Inquiry{
+		CreatedDateTime: time.Now(),
+		CookID:          item.CookID,
+		EaterID:         eaterID,
+		EaterPhotoURL:   eater.PhotoURL,
+		EaterName:       eater.Name,
+		ItemID:          itemID,
+		Item: ItemInfo{
+			Name:            item.Title,
+			Description:     item.Description,
+			PhotoURL:        itemPhoto,
+			Ingredients:     item.Ingredients,
+			DietaryConcerns: item.DietaryConcerns,
+		},
+		State:                    State.Pending,
+		EaterAction:              EaterAction.Accepted,
+		CookAction:               CookAction.Pending,
+		BTTransactionID:          transactionID,
+		ExpectedExchangeDateTime: expectedExchangeTime,
+		CookPricePerServing:      item.CookPricePerServing,
+		Servings:                 numServings,
+		PaymentInfo: PaymentInfo{
+			Price:         totalCookPrice,
+			ExchangePrice: exchangePrice,
+			TaxPrice:      totalTaxPrice,
+			ServiceFee:    totalGigaFee,
+			TotalPrice:    totalPrice,
+		},
+		ExchangeMethod: int32(exchangeMethod),
+		ExchangePlanInfo: ExchangePlanInfo{
+			EaterAddress: *eaterAddress,
+			CookAddress:  cook.Address,
+			Distance:     distance,
+			Duration:     duration,
+		},
+	}
+	_, err = putIncomplete(c.ctx, inquiry)
+	if err != nil {
+		_, pErr := paymentC.RefundSale(transactionID)
+		if pErr != nil {
+			utils.Criticalf(c.ctx, "BT Transaction (%s) was not voided! Err: %+v", transactionID, pErr)
+		}
+		return nil, errDatastore.WithError(err).Wrap("cannot putIncomplete inquiry")
+	}
+	// TODO add task to where cook has (exchangeTime || 12 hours) to reply
+	return inquiry, nil
+}
+
+// CookAccept updates the Inquiry to Cook Accepted.
+func (c *Client) CookAccept(user *types.User, id int64) (*Inquiry, error) {
+	inquiry, err := get(c.ctx, id)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get Inquiry(%d)", id)
+	}
+	if !user.IsAdmin() || user.ID != inquiry.CookID {
+		return nil, errUnauthorized.WithMessage("You are not the cook of the Inquiry.")
+	}
+	if inquiry.State != State.Pending {
+		return nil, errInvalidParameter.WithMessage("Inquiry is no long in a pending state.")
+	}
+	if inquiry.CookAction != CookAction.Accepted {
+		inquiry.CookAction = CookAction.Accepted
+		if inquiry.EaterAction == EaterAction.Accepted {
+			// submit transaction for settlement
+			paymentC := getPaymentClient(c.ctx)
+			err = paymentC.SubmitForSettlement(inquiry.BTTransactionID)
+			if err != nil {
+				return nil, errors.Wrap("failed to payment.SubmitForSettlement", err)
+			}
+			// TODO notify eater and send twilio status message to channel
+		}
+		err = put(c.ctx, id, inquiry)
+		if err != nil {
+			return nil, errDatastore.WithError(err).Wrapf("failed to put Inquiry(%d) after submitting for settlement", id)
+		}
+	}
+	return inquiry, nil
+}
+
+// CookDecline updates the Inquiry to Cook Declined.
+func (c *Client) CookDecline(user *types.User, id int64) (*Inquiry, error) {
+	inquiry, err := get(c.ctx, id)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get Inquiry(%d)", id)
+	}
+	if !user.IsAdmin() || user.ID != inquiry.CookID {
+		return nil, errUnauthorized.WithMessage("You are not the cook of the Inquiry.")
+	}
+	if inquiry.State != State.Pending {
+		return nil, errInvalidParameter.WithMessage("Inquiry is no long in a pending state.")
+	}
+	if inquiry.CookAction != CookAction.Declined {
+		inquiry.CookAction = CookAction.Declined
+		// submit transaction for refund
+		paymentC := getPaymentClient(c.ctx)
+		var refundID string
+		refundID, err = paymentC.RefundSale(inquiry.BTTransactionID)
+		if err != nil {
+			return nil, errors.Wrap("failed to payment.SubmitForSettlement", err)
+		}
+		inquiry.BTRefundTransactionID = refundID
+		// TODO notify eater and send twilio status message to channel
+		err = put(c.ctx, id, inquiry)
+		if err != nil {
+			return nil, errDatastore.WithError(err).Wrapf("failed to put Inquiry(%d) after submitting for settlement", id)
+		}
+	}
+	return inquiry, nil
+}
+
+// CookCancel
+// func (c *Client) CookCancel() error {
+
+// 	return nil
+// }
+
+// EaterCancel updates the Inquiry to Eater Canceled.
+func (c *Client) EaterCancel(user *types.User, id int64) (*Inquiry, error) {
+	inquiry, err := get(c.ctx, id)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to get Inquiry(%d)", id)
+	}
+	if !user.IsAdmin() || user.ID != inquiry.EaterID {
+		return nil, errUnauthorized.WithMessage("You are not part of the Inquiry.")
+	}
+	if inquiry.State != State.Pending {
+		return nil, errInvalidParameter.WithMessage("Inquiry is no long in a pending state.")
+	}
+	if inquiry.ExpectedExchangeDateTime.Sub(time.Now()) > time.Duration(12)*time.Hour {
+		return nil, errInvalidParameter.WithMessage("The Inquiry can no longer be canceled.")
+	}
+	inquiry.State = State.Refunded
+	inquiry.EaterAction = EaterAction.Canceled
+	// submit transaction for refund
+	paymentC := getPaymentClient(c.ctx)
+	var refundID string
+	refundID, err = paymentC.RefundSale(inquiry.BTTransactionID)
+	if err != nil {
+		return nil, errors.Wrap("failed to payment.SubmitForSettlement", err)
+	}
+	inquiry.BTRefundTransactionID = refundID
+	// TODO notify eater and send twilio status message to channel
+	err = put(c.ctx, id, inquiry)
+	if err != nil {
+		return nil, errDatastore.WithError(err).Wrapf("failed to put Inquiry(%d) after submitting for settlement", id)
+	}
+	return inquiry, nil
+}
+
+// EaterRequestRefund
+// TODO Add request refund feature
+
+// CookAutoDecline
+
+// Process
+
+var getPaymentClient = func(ctx context.Context) paymentClient {
+	return payment.New(ctx)
+}
+
+type paymentClient interface {
+	StartSale(string, string, float32, float32) (string, error)
+	SubmitForSettlement(string) error
+	ReleaseSale(string) (string, error)
+	RefundSale(string) (string, error)
+	CancelRelease(string) (string, error)
+}
+
+var getItemClient = func(ctx context.Context) itemClient {
+	return item.New(ctx)
+}
+
+type itemClient interface {
+	Get(int64) (*item.Item, error)
+}
+
+var getEaterClient = func(ctx context.Context) eaterClient {
+	return eater.New(ctx)
+}
+
+type eaterClient interface {
+	Get(string) (*eater.Eater, error)
+}
+
+var getCookClient = func(ctx context.Context) cookClient {
+	return cook.New(ctx)
+}
+
+type cookClient interface {
+	Get(string) (*cook.Cook, error)
+}
---
+++ corenew/inquiry/types.go
@@ -0,0 +1,94 @@
+package inquiry
+
+import (
+	"time"
+
+	"github.com/atishpatel/Gigamunch-Backend/corenew/item"
+	"github.com/atishpatel/Gigamunch-Backend/types"
+)
+
+const (
+	kindInquiry = "Inquiry"
+)
+
+// State is the possible states for an Inquiry.
+var State = struct{ Pending, RefundRequested, Refunded, Fulfilled, Paid string }{
+	Pending:         "Pending",
+	RefundRequested: "RefundRequested",
+	Refunded:        "Refunded",
+	Fulfilled:       "Fulfilled",
+	Paid:            "Paid",
+}
+
+// EaterAction is the possible states for an Eater in regards to an Inquiry.
+var EaterAction = struct{ Pending, Accepted, Canceled, RefundRequested string }{
+	Pending:         "Pending",
+	Accepted:        "Accepted",
+	Canceled:        "Canceled",
+	RefundRequested: "RefundRequested",
+}
+
+// CookAction is the possible states for a Cook in regards to an Inquiry.
+var CookAction = struct{ Pending, Accepted, Declined, Canceled, Refunded string }{
+	Pending:  "Pending",
+	Accepted: "Accepted",
+	Declined: "Declined",
+	Canceled: "Canceled",
+	Refunded: "Refunded",
+}
+
+// PaymentInfo is the payment information related to an Inquiry.
+type PaymentInfo struct {
+	Price         float32 `json:"price" datastore:",noindex"`
+	ExchangePrice float32 `json:"exchange_price" datastore:",noindex"`
+	TaxPrice      float32 `json:"tax_price" datastore:",noindex"`
+	ServiceFee    float32 `json:"service_fee" datastore:",noindex"`
+	TotalPrice    float32 `json:"total_price" datastore:",noindex"`
+}
+
+// ItemInfo contains information about the Item related to an Inquiry.
+type ItemInfo struct {
+	Name            string               `json:"name" datastore:",noindex"`
+	Description     string               `json:"description" datastore:",noindex"`
+	PhotoURL        string               `json:"photo_url" datastore:",noindex"`
+	Ingredients     []string             `json:"ingredients" datastore:",noindex"`
+	DietaryConcerns item.DietaryConcerns `json:"dietary_concerns" datastore:",noindex"`
+}
+
+// ExchangePlanInfo is the basic information need for an exchange plan.
+type ExchangePlanInfo struct {
+	EaterAddress types.Address `json:"eater_address" datastore:",noindex"`
+	CookAddress  types.Address `json:"cook_address" datastore:",noindex"`
+	Distance     float32       `json:"distance" datastore:",noindex"`
+	Duration     int64         `json:"duration" datastore:",noindex"`
+}
+
+// Inquiry is an Inquiry made about an Item by an Eater.
+type Inquiry struct {
+	ID              int64     `json:"id,string" datastore:",noindex"`
+	CreatedDateTime time.Time `json:"created_datetime" datastore:",index"`
+	CookID          string    `json:"cook_id" datastore:",index"`
+	EaterID         string    `json:"eater_id" datastore:",index"`
+	EaterPhotoURL   string    `json:"eater_photo_url" datastore:",noindex"`
+	EaterName       string    `json:"eater_name" datastore:",noindex"`
+	ReviewID        int64     `json:"review_id" datastore:",noindex"`
+	ItemID          int64     `json:"item_id" datastore:",index"`
+	Item            ItemInfo  `json:"item" datastore:",noindex"`
+	MarkedAsDone    bool      `json:"marked_as_done" datastore:",index"`
+	State           string    `json:"state" datastore:",noindex"`
+	EaterAction     string    `json:"eater_action" datastore:",noindex"`
+	CookAction      string    `json:"cook_action" datastore:",noindex"`
+	Issue           bool      `json:"issue" datastore:",noindex"`
+
+	// Braintree info
+	BTTransactionID       string `json:"bt_transaction_id" datastore:",index"`
+	BTRefundTransactionID string `json:"bt_refund_transaction_id" datastore:",noindex"`
+
+	// Exchange Info
+	ExpectedExchangeDateTime time.Time        `json:"expected_exchange_datetime" datastore:",index"`
+	CookPricePerServing      float32          `json:"cook_price_per_serving" datastore:",noindex"`
+	Servings                 int32            `json:"servings" datastore:",noindex"`
+	PaymentInfo              PaymentInfo      `json:"payment_info" datastore:",noindex"`
+	ExchangeMethod           int32            `json:"exchange_method" datastore:",noindex"`
+	ExchangePlanInfo         ExchangePlanInfo `json:"exchange_plan_info" datastore:",noindex"`
+}
