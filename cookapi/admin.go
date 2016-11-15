bf22e912692dfb503a97ac41310ba414b893b813
Atish Patel <atishpatel2012@gmail.com>
2016-11-15 14:50:03 -0600

    cookapi: Added AddToProcessInquiryQueue for admin

--- cookapi/admin.go
+++ cookapi/admin.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
+
 	"golang.org/x/net/context"
 
 	"github.com/atishpatel/Gigamunch-Backend/auth"
+	"github.com/atishpatel/Gigamunch-Backend/corenew/tasks"
 	"github.com/atishpatel/Gigamunch-Backend/errors"
 )
 
@@ -32,5 +35,36 @@ func (service *Service) CreateFakeGigatoken(ctx context.Context, req *CreateFake
 		return resp, nil
 	}
 	resp.Gigatoken = gigatoken
+	return resp, nil
+}
+
+// AddToProcessInquiryQueueReq is the request for AddToProcessInquiryQueue.
+type AddToProcessInquiryQueueReq struct {
+	IDReq
+	Hours int `json:"hours"`
+}
+
+// AddToProcessInquiryQueue adds a process to the inquiry queue.
+func (service *Service) AddToProcessInquiryQueue(ctx context.Context, req *AddToProcessInquiryQueueReq) (*RefreshTokenResp, error) {
+	resp := new(RefreshTokenResp)
+	defer handleResp(ctx, "AddToProcessInquiryQueue", resp.Err)
+	user, err := validateRequestAndGetUser(ctx, req)
+	if err != nil {
+		resp.Err = errors.GetErrorWithCode(err)
+		return resp, nil
+	}
+	if !user.IsAdmin() {
+		resp.Err = errors.ErrorWithCode{Code: errors.CodeUnauthorizedAccess, Message: "User is not an admin."}
+		return resp, nil
+	}
+
+	at := time.Now().Add(time.Duration(req.Hours) * time.Hour)
+	tasksC := tasks.New(ctx)
+	err = tasksC.AddProcessInquiry(req.ID, at)
+	if err != nil {
+		resp.Err = errors.GetErrorWithCode(err).Wrap("failed to tasks.AddProcessInquiry")
+		return resp, nil
+	}
+
 	return resp, nil
 }
