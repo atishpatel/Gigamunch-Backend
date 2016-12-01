5e9f3efbc61f9e9d0206a4f987e6a261e765d15d
Atish Patel <atishpatel2012@gmail.com>
2016-11-30 22:50:49 -0600

    server: removed gigachef web app

--- server/gigachef/sw-precache-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  staticFileGlobs: [
-    '/gigachef/index.html',
-    '/gigachef/manifest.json',
-    '/gigachef/bower_components/webcomponentsjs/webcomponents-lite.min.js'
-  ],
-  navigateFallback: '/gigachef/index.html'
-};
