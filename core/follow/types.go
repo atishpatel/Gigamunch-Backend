2fe1c6bfec211b143c78b97c51b1f0d63c0edb4b
Atish Patel <atishpatel2012@gmail.com>
2017-04-27 16:26:04 -0500

    removed old(megamunch) core and endpoint code

--- core/follow/types.go
+++
@@ -1,9 +0,0 @@
-package follow
-
-import "time"
-
-type Follow struct {
-	FollowDateTime time.Time `json:"follow_datetime"`
-	FollowerID     string    `json:"follower_id"`
-	FollowingID    string    `json:"following_id"`
-}
