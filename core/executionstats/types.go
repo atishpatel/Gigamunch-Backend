804e28a57a2552de75989696e9b9714a012a7565
Atish Patel <atishpatel2012@gmail.com>
2018-04-04 17:27:41 -0500

    added Get,GetAll, and Update for executionstats

--- core/executionstats/types.go
+++ core/executionstats/types.go
@@ -1,20 +1,38 @@
 package executionstats
 
+import (
+	"time"
+
+	"github.com/atishpatel/Gigamunch-Backend/core/common"
+)
+
+// Kind is the kind for datastore.
+const Kind = "ExecutionStats"
+
 // ExecutionStats is the status related to a culture execution.
 type ExecutionStats struct {
-	Payroll         []Payroll
-	Revenue         float32
-	FoodCosts       float32
-	DeliveryCosts   float32
-	OnboardingCosts float32
-	// TODO: add other fields
-	OtherCosts float32
+	ID              int64           `json:"id,omitempty"`
+	CreatedDatetime time.Time       `json:"created_datetime,omitempty"`
+	Date            string          `json:"date,omitempty"`
+	Location        common.Location `json:"location,omitempty"`
+	Country         string          `json:"country,omitempty"`
+	City            string          `json:"city,omitempty"`
+	Revenue         float32         `json:"revenue,omitempty"`
+	Payroll         []Payroll       `json:"payroll,omitempty"`
+	PayrollCosts    float32         `json:"payroll_costs,omitempty"`
+	FoodCosts       float32         `json:"food_costs,omitempty"`
+	DeliveryCosts   float32         `json:"delivery_costs,omitempty"`
+	OnboardingCosts float32         `json:"onboarding_costs,omitempty"`
+	ProcessingCosts float32         `json:"processing_costs,omitempty"`
+	TaxCosts        float32         `json:"tax_costs,omitempty"`
+	PackagingCosts  float32         `json:"packaging_costs,omitempty"`
+	OtherCosts      float32         `json:"other_costs,omitempty"`
 }
 
 // Payroll is the payroll.
 type Payroll struct {
-	Name string
-	// TODO: hours as time slots?
-	Hours   int
-	Postion string // Headcook, Linecook, Prepcook, etc
+	Name    string  `json:"name,omitempty"`
+	Hours   float32 `json:"hours,omitempty"`
+	Wage    float32 `json:"wage,omitempty"`
+	Postion string  `json:"postion,omitempty"` // Headcook, Linecook, Prepcook, etc
 }
