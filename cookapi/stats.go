fab8a70e751a9e032105252252bd42ce5b4b93d8
Atish Patel <atishpatel2012@gmail.com>
2018-04-22 09:20:57 -0500

    init stats WIP

--- cookapi/stats.go
+++ cookapi/stats.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/atishpatel/Gigamunch-Backend/corenew/sub"
 	"github.com/atishpatel/Gigamunch-Backend/errors"
 	"golang.org/x/net/context"
@@ -8,7 +10,6 @@ import (
 
 // CohortCell is a cell.
 type CohortCell struct {
-	Interval         int16   `json:"interval,omitempty"`
 	AmountLeft       int     `json:"amount_left,omitempty"`
 	AmountLost       int     `json:"amount_lost,omitempty"`
 	RetentionPercent float32 `json:"retention_percent,omitempty"`
@@ -16,19 +17,21 @@ type CohortCell struct {
 
 // CohortRow is a row.
 type CohortRow struct {
-	StartDate  string       `json:"start_date,omitempty"`
-	CohortCell []CohortCell `json:"cohort_cell,omitempty"`
+	Label      string        `json:"label,omitempty"`
+	CohortCell []*CohortCell `json:"cohort_cell,omitempty"`
 }
 
 // CohortAnalysis is a full cohortAnalysis.
 type CohortAnalysis struct {
-	CohortRows []CohortRow `json:"cohort_rows,omitempty"`
+	CohortRows []*CohortRow `json:"cohort_rows,omitempty"`
+	Interval   int16        `json:"interval,omitempty"`
 }
 
 // GetGeneralStatsResp is a response for GetGeneralStats.
 type GetGeneralStatsResp struct {
-	Activities           []*sub.SublogSummary `json:"activities"`
-	WeeklyCohortAnalysis *CohortAnalysis      `json:"weekly_cohort_analysis"`
+	// Activities           []*sub.SublogSummary `json:"activities"`
+	WeeklyCohortAnalysis  *CohortAnalysis `json:"weekly_cohort_analysis"`
+	PercentCohortAnalysis *CohortAnalysis `json:"percent_cohort_analysis"`
 	ErrorOnlyResp
 }
 
@@ -58,13 +61,47 @@ func (service *Service) GetGeneralStats(ctx context.Context, req *GigatokenReq) 
 		resp.Err = errors.GetErrorWithCode(err).Wrap("failed to sub.GetSublogSummaries")
 		return resp, nil
 	}
-	resp.Activities = activities
+	// resp.Activities = activities
 	resp.WeeklyCohortAnalysis = getWeeklyCohort(activities)
 	return resp, nil
 }
 
 func getWeeklyCohort(activities []*sub.SublogSummary) *CohortAnalysis {
-	analysis := new(CohortAnalysis)
-
+	analysis := &CohortAnalysis{
+		Interval: 7,
+	}
+	if len(activities) == 0 {
+		return analysis
+	}
+	// setup for first row
+	lastMinDate := activities[0].MinDate
+	row := &CohortRow{
+		Label: lastMinDate.String(),
+	}
+	// for every activity
+	for i := 0; i < len(activities); i++ {
+		// next cohort row
+		if activities[i].MinDate.Sub(lastMinDate) > 12*time.Hour {
+			analysis.CohortRows = append(analysis.CohortRows, row)
+			lastMinDate = activities[i].MinDate
+			row = &CohortRow{
+				Label: lastMinDate.String(),
+			}
+		}
+		for j := 0; j < activities[i].NumTotal; j++ {
+			if len(row.CohortCell)-1 < j {
+				row.CohortCell = append(row.CohortCell, new(CohortCell))
+			}
+			row.CohortCell[j].AmountLeft++
+		}
+	}
+	// fill in amount lost and retention percent
+	for _, r := range analysis.CohortRows {
+		startAmount := r.CohortCell[0].AmountLeft
+		for _, c := range r.CohortCell {
+			c.AmountLost = startAmount - c.AmountLeft
+			c.RetentionPercent = float32(c.AmountLeft) / float32(startAmount)
+		}
+	}
 	return analysis
 }
