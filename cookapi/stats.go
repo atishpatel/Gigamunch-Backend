fd4d4d796585c8fc87d06ab5140cc6206ac38fdb
Atish Patel <atishpatel2012@gmail.com>
2018-04-23 15:43:14 -0500

    added monthly cohort

--- cookapi/stats.go
+++ cookapi/stats.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/atishpatel/Gigamunch-Backend/corenew/sub"
@@ -29,9 +30,10 @@ type CohortAnalysis struct {
 
 // GetGeneralStatsResp is a response for GetGeneralStats.
 type GetGeneralStatsResp struct {
-	// Activities           []*sub.SublogSummary `json:"activities"`
-	WeeklyCohortAnalysis  *CohortAnalysis `json:"weekly_cohort_analysis"`
-	PercentCohortAnalysis *CohortAnalysis `json:"percent_cohort_analysis"`
+	Activities            []*sub.SublogSummary `json:"activities"`
+	WeeklyCohortAnalysis  *CohortAnalysis      `json:"weekly_cohort_analysis"`
+	MonthlyCohortAnalysis *CohortAnalysis      `json:"monthly_cohort_analysis"`
+	PercentCohortAnalysis *CohortAnalysis      `json:"percent_cohort_analysis"`
 	ErrorOnlyResp
 }
 
@@ -63,6 +65,7 @@ func (service *Service) GetGeneralStats(ctx context.Context, req *GigatokenReq) 
 	}
 	// resp.Activities = activities
 	resp.WeeklyCohortAnalysis = getWeeklyCohort(activities)
+	resp.MonthlyCohortAnalysis = getMonthlyCohort(activities)
 	return resp, nil
 }
 
@@ -81,11 +84,58 @@ func getWeeklyCohort(activities []*sub.SublogSummary) *CohortAnalysis {
 	// for every activity
 	for i := 0; i < len(activities); i++ {
 		// next cohort row
-		if activities[i].MinDate.Sub(lastMinDate) > 12*time.Hour {
+		diff := activities[i].MinDate.Sub(lastMinDate)
+		if diff < time.Duration(0) {
+			diff *= -1
+		}
+		if diff > 12*time.Hour {
 			analysis.CohortRows = append(analysis.CohortRows, row)
 			lastMinDate = activities[i].MinDate
 			row = &CohortRow{
 				Label: lastMinDate.String(),
+			}
+		}
+		for j := 0; j < activities[i].NumTotal; j++ {
+			if len(row.CohortCell)-1 < j {
+				row.CohortCell = append(row.CohortCell, new(CohortCell))
+			}
+			row.CohortCell[j].AmountLeft++
+		}
+	}
+	// fill in amount lost and retention percent
+	for _, r := range analysis.CohortRows {
+		startAmount := r.CohortCell[0].AmountLeft
+		for _, c := range r.CohortCell {
+			c.AmountLost = startAmount - c.AmountLeft
+			c.RetentionPercent = float32(c.AmountLeft) / float32(startAmount)
+		}
+	}
+	return analysis
+}
+
+// Assumes the activities are sorted by min date.
+func getMonthlyCohort(activities []*sub.SublogSummary) *CohortAnalysis {
+	analysis := &CohortAnalysis{
+		Interval: 30,
+	}
+	if len(activities) == 0 {
+		return analysis
+	}
+	const labelFormat = "%s-%d"
+	// setup for first row
+	lastMonthYear := fmt.Sprintf(labelFormat, activities[0].MinDate.Month().String(), activities[0].MinDate.Year())
+	row := &CohortRow{
+		Label: lastMonthYear,
+	}
+	// for every activity
+	for i := 0; i < len(activities); i++ {
+		// next cohort row
+		monthYear := fmt.Sprintf(labelFormat, activities[i].MinDate.Month().String(), activities[i].MinDate.Year())
+		if monthYear != lastMonthYear {
+			analysis.CohortRows = append(analysis.CohortRows, row)
+			lastMonthYear = monthYear
+			row = &CohortRow{
+				Label: lastMonthYear,
 			}
 		}
 		for j := 0; j < activities[i].NumTotal; j++ {
