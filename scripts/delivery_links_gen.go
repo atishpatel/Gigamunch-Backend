780845cafb9efd7f458c1ca24bc2b248902acd8b
Atish Patel <atishpatel2012@gmail.com>
2018-07-03 16:25:23 -0500

    switched to new gigakitchen for delivery route optimizations and links gen

--- scripts/delivery_links_gen.go
+++ scripts/delivery_links_gen.go
@@ -69,7 +69,7 @@ var (
 
 func main() {
 	fmt.Printf("Doc link:\n%s\n----------\n", docSheet)
-	startAddress := []string{"166+Chesapeake+Harbor+Blvd,+Hendersonville"}
+	startAddress := []string{"1001+Thompson+Pl,+Nashville,+TN+37217"}
 	del := "\n"
 	replacer := strings.NewReplacer("\t", "", " ", "+")
 	for _, i := range input {
