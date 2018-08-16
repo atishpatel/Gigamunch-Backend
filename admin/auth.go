bf0d3b969451457d13d734bd608f9ad9f959a2fb
Atish Patel <atishpatel2012@gmail.com>
2018-08-16 11:01:32 -0500

    updated decoding method

--- admin/auth.go
+++ admin/auth.go
@@ -2,14 +2,12 @@ package admin
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 
 	pb "github.com/atishpatel/Gigamunch-Backend/Gigamunch-Proto/admin"
 	"github.com/atishpatel/Gigamunch-Backend/core/auth"
 	"github.com/atishpatel/Gigamunch-Backend/core/logging"
 	"github.com/atishpatel/Gigamunch-Backend/errors"
-	"github.com/gorilla/schema"
 )
 
 // Login takes a Firebase Token and returns an auth token.
@@ -42,23 +40,14 @@ func (s *server) Login(ctx context.Context, w http.ResponseWriter, r *http.Reque
 func (s *server) Refresh(ctx context.Context, w http.ResponseWriter, r *http.Request, log *logging.Client) Response {
 	req := new(pb.TokenOnlyReq)
 	var err error
+
 	// decode request
-	if r.Method == "GET" {
-		decoder := schema.NewDecoder()
-		err := decoder.Decode(req, r.URL.Query())
-		if err != nil {
-			return failedToDecode(err)
-		}
-	} else {
-		decoder := json.NewDecoder(r.Body)
-		err = decoder.Decode(&req)
-		if err != nil {
-			return failedToDecode(err)
-		}
-		defer closeRequestBody(r)
+	err = decodeRequest(ctx, r, &req)
+	if err != nil {
+		return failedToDecode(err)
 	}
-	logging.Infof(ctx, "Request: %+v", req)
 	// end decode request
+
 	authC, err := auth.NewClient(ctx, log, s.db, s.sqlDB, s.serverInfo)
 	if err != nil {
 		return errors.Annotate(err, "failed to get auth.NewClient")
