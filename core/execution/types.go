2fa3cce02f26ef12599833d5d041039f9d639f92
Atish Patel <atishpatel2012@gmail.com>
2018-11-01 17:23:31 -0500

    execution types updated

--- core/execution/types.go
+++ core/execution/types.go
@@ -21,12 +21,13 @@ type Execution struct {
 	Publish         bool            `json:"publish"`
 	CreatedDatetime time.Time       `json:"created_datetime"`
 	// Info
-	Culture       *Culture       `json:"culture"`
-	Content       *Content       `json:"content"`
-	CultureCook   *CultureCook   `json:"culture_cook"`
-	CultureGuide  *CultureGuide  `json:"culture_guide"`
-	Dishes        []*Dish        `json:"dishes"`
-	Notifications *Notifications `json:"notifications"`
+	Culture       Culture       `json:"culture"`
+	Content       Content       `json:"content"`
+	CultureCook   CultureCook   `json:"culture_cook"`
+	CultureGuide  CultureGuide  `json:"culture_guide"`
+	Dishes        []Dish        `json:"dishes"`
+	Stickers      []Sticker     `json:"stickers"`
+	Notifications Notifications `json:"notifications"`
 	// Diet
 	HasPork    bool `json:"has_pork"`
 	HasBeef    bool `json:"has_beef"`
@@ -49,60 +50,67 @@ type InfoBox struct {
 
 // CultureGuide is content related to the culture guide.
 type CultureGuide struct {
-	InfoBoxes          []*InfoBox `json:"info_boxes" datastore:",noindex"`
-	DinnerInstructions string     `json:"dinner_instructions" datastore:",noindex"`
-	MainColor          string     `json:"main_color" datastore:",noindex"`
-	FontName           string     `json:"font_name" datastore:",noindex"`
-	FontStyle          string     `json:"font_style" datastore:",noindex"`
-	FontCaps           bool       `json:"font_caps" datastore:",noindex"`
+	InfoBoxes                    []InfoBox `json:"info_boxes" datastore:",noindex"`
+	DinnerInstructions           string    `json:"dinner_instructions" datastore:",noindex"`
+	MainColor                    string    `json:"main_color" datastore:",noindex"`
+	FontName                     string    `json:"font_name" datastore:",noindex"`
+	FontStyle                    string    `json:"font_style" datastore:",noindex"`
+	FontCaps                     bool      `json:"font_caps" datastore:",noindex"`
+	FontNamePostScript           string    `json:"font_name_post_script" datastore:",noindex"`
+	VegetarianDinnerInstructions string    `json:"vegetarian_dinner_instructions" datastore:",noindex"`
 }
 
 // Content is a collection of urls pointing to content realted to the execution.
 type Content struct {
-	HeroImageURL             string `json:"hero_image_url" datastore:",noindex"`
+	LandscapeImageURL        string `json:"landscape_image_url" datastore:",noindex"`
 	CookImageURL             string `json:"cook_image_url" datastore:",noindex"`
 	HandsPlateNonVegImageURL string `json:"hands_plate_non_veg_image_url" datastore:",noindex"`
 	HandsPlateVegImageURL    string `json:"hands_plate_veg_image_url" datastore:",noindex"`
-	DinnerImageURL           string `json:"dinner_image_url" datastore:",noindex"`
+	DinnerNonVegImageURL     string `json:"dinner_non_veg_image_url" datastore:",noindex"`
+	DinnerVegImageURL        string `json:"dinner_veg_image_url" datastore:",noindex"`
 	SpotifyURL               string `json:"spotify_url" datastore:",noindex"`
 	YoutubeURL               string `json:"youtube_url" datastore:",noindex"`
-	FontURL                  string
+	FontURL                  string `json:"font_url" datastore:",noindex"`
 }
 
 // Culture is the culture in a culture execution.
 type Culture struct {
-	Country     string `json:"country"`
-	City        string `json:"city"`
-	Description string `json:"description" datastore:",noindex"`
-	Nationality string `json:"nationality" datastore:",noindex"`
-	Greeting    string `json:"greeting" datastore:",noindex"`
-	FlagEmoji   string `json:"flag_emoji" datastore:",noindex"`
+	Country            string `json:"country"`
+	City               string `json:"city"`
+	DescriptionPreview string `json:"description_preview" datastore:",noindex"`
+	Description        string `json:"description" datastore:",noindex"`
+	Nationality        string `json:"nationality" datastore:",noindex"`
+	Greeting           string `json:"greeting" datastore:",noindex"`
+	FlagEmoji          string `json:"flag_emoji" datastore:",noindex"`
 }
 
 // Sticker are reheat stickers for dishes.
 type Sticker struct {
-	Name                string `json:"name"`
-	Ingredients         string `json:"ingredients"`
-	ExtraInstructions   string `json:"extra_instructions"`
-	ReheatOption1       string `json:"reheat_option_1"`
-	ReheatOption2       string `json:"reheat_option_2"`
-	ReheatTime1         string `json:"reheat_time_1"`
-	ReheatTime2         string `json:"reheat_time_2"`
-	ReheatInstructions1 string `json:"reheat_instructions_1"`
-	ReheatInstructions2 string `json:"reheat_instructions_2"`
-	EatingTemperature   string `json:"eating_temperature"`
+	Name                   string `json:"name"`
+	Ingredients            string `json:"ingredients"`
+	ExtraInstructions      string `json:"extra_instructions"`
+	ReheatOption1          string `json:"reheat_option_1" datastore:",noindex"`
+	ReheatOption2          string `json:"reheat_option_2" datastore:",noindex"`
+	ReheatOption1Preferred bool   `json:"reheat_option_1_preferred" datastore:",noindex"`
+	ReheatTime1            string `json:"reheat_time_1" datastore:",noindex"`
+	ReheatTime2            string `json:"reheat_time_2" datastore:",noindex"`
+	ReheatInstructions1    string `json:"reheat_instructions_1" datastore:",noindex"`
+	ReheatInstructions2    string `json:"reheat_instructions_2" datastore:",noindex"`
+	EatingTemperature      string `json:"eating_temperature" datastore:",noindex"`
 }
 
 // Dish is a dish in a culture execution.
 type Dish struct {
-	Number             int32      `json:"number" datastore:",noindex"`
-	Color              string     `json:"color"`
-	Name               string     `json:"name"`
-	Description        string     `json:"description" datastore:",noindex"`
-	Ingredients        string     `json:"ingredients" datastore:",noindex"`
-	IsForVegetarian    bool       `json:"is_for_vegetarian"`
-	IsForNonVegetarian bool       `json:"is_for_non_vegetarian"`
-	Stickers           []*Sticker `json:"stickers"`
+	Number             int32  `json:"number" datastore:",noindex"`
+	Color              string `json:"color"`
+	Name               string `json:"name"`
+	DescriptionPreview string `json:"description_preview" datastore:",noindex"`
+	Description        string `json:"description" datastore:",noindex"`
+	Ingredients        string `json:"ingredients" datastore:",noindex"`
+	IsForVegetarian    bool   `json:"is_for_vegetarian"`
+	IsForNonVegetarian bool   `json:"is_for_non_vegetarian"`
+	IsOnMainPlate      bool   `json:"is_on_main_plate" datastore:",noindex"`
+	ImageURL           string `json:"image_url" datastore:",noindex"`
 }
 
 // QandA are questions and answers with the culture cook.
@@ -113,9 +121,9 @@ type QandA struct {
 
 // CultureCook is the culture cook for a culture execution.
 type CultureCook struct {
-	FirstName    string   `json:"first_name,omitempty"`
-	LastName     string   `json:"last_name,omitempty"`
-	Story        string   `json:"story,omitempty" datastore:",noindex"`
-	StoryPreview string   `json:"story_preview" datastore:",noindex"`
-	QandA        []*QandA `json:"q_and_a" datastore:",noindex"`
+	FirstName    string  `json:"first_name,omitempty"`
+	LastName     string  `json:"last_name,omitempty"`
+	Story        string  `json:"story,omitempty" datastore:",noindex"`
+	StoryPreview string  `json:"story_preview" datastore:",noindex"`
+	QandA        []QandA `json:"q_and_a" datastore:",noindex"`
 }
