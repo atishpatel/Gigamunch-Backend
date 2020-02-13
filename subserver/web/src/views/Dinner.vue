de27fbbb1f22827f16bae7e1abc933dc6326eabc
Chris <chrissipe12@gmail.com>
2020-02-13 12:39:55 -0600

    work in progress

--- subserver/web/src/views/Dinner.vue
+++ subserver/web/src/views/Dinner.vue
@@ -6,12 +6,6 @@
       :activity="activity"
       :userSummary="userSummary"
     ></DinnerPublished>
-    <div class="hero-image unset-link">
-      <img
-        :src="landscape_image_src"
-        :alt="computedLandscapeImageAlt"
-      >
-    </div>
 
   </div>
 </template>
@@ -70,30 +64,12 @@ export default class Dinner extends Vue {
   get computedLandscapeImageAlt() {
     return this.exe.culture.country + 'landscape image';
   }
-  get landscape_image_src() {
-    return this.exe.content.landscape_image_url;
+  get landscapeImageSrc() {
+    return this.exe.email.landscape_image_url;
   }
 }
 </script>
+
+
 <style scoped lang="scss">
-.hero-image {
-  display: block;
-  width: 100%;
-  position: relative;
-  height: 0;
-  padding: 56.25% 0 0 0;
-  overflow: hidden;
-  background-color: white;
-  border-radius: 5px;
-  img {
-    position: absolute;
-    display: block;
-    width: 100%;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-  }
-}
 </style>
