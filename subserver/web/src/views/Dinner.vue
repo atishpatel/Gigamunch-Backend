8fa2b6af73de85c40b625e0317c309b9bb96d442
Chris <chrissipe12@gmail.com>
2020-02-13 16:02:56 -0600

    got rid of useless code

--- subserver/web/src/views/Dinner.vue
+++ subserver/web/src/views/Dinner.vue
@@ -61,12 +61,6 @@ export default class Dinner extends Vue {
       this.activity = resp.execution_and_activity.activity;
     });
   }
-  get computedLandscapeImageAlt() {
-    return this.exe.culture.country + 'landscape image';
-  }
-  get landscapeImageSrc() {
-    return this.exe.email.landscape_image_url;
-  }
 }
 </script>
 
