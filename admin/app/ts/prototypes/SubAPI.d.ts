c5e619a04e6061ff06d417d12055988016e71f98
Chris <chrissipe12@gmail.com>
2020-02-13 20:23:42 -0600

    built proto

--- admin/app/ts/prototypes/SubAPI.d.ts
+++ admin/app/ts/prototypes/SubAPI.d.ts
@@ -1,47 +1,44 @@
 declare namespace SubAPI {
 interface GetUserSummaryReq {
-undefined: undefined
 }
 interface GetUserSummaryResp {
-  error: Common.Error
-  // is an active subscriber
-  is_active: boolean
-  on_probation: boolean
-  // has finished checkout flow
-  has_subscribed: boolean
-  is_logged_in: boolean
+    error: Common.Error
+    // is an active subscriber
+    is_active: boolean
+    on_probation: boolean
+    // has finished checkout flow
+    has_subscribed: boolean
+    is_logged_in: boolean
 }
 interface GetAccountInfoReq {
-undefined: undefined
 }
 interface GetAccountInfoResp {
-  error: Common.Error
-  email_prefs: Common.EmailPref[]
-  phone_prefs: Common.PhonePref[]
-  address: Common.Address
-  payment_info: PaymentInfo
+    error: Common.Error
+    email_prefs: Common.EmailPref[]
+    phone_prefs: Common.PhonePref[]
+    address: Common.Address
+    payment_info: PaymentInfo
 }
 interface PaymentInfo {
-  card_number_preview: string
-  card_type: string
+    card_number_preview: string
+    card_type: string
 }
 interface GetExecutionsReq {
-  start: number
-  limit: number
+    start: number
+    limit: number
 }
 interface GetExecutionsDateReq {
     date: string
 }
 interface GetExecutionsResp {
-  error: Common.Error
-  execution_and_activity: Common.ExecutionAndActivity[]
-  activities: Common.Activity[]
+    error: Common.Error
+    execution_and_activity: Common.ExecutionAndActivity[]
 }
 interface GetExecutionReq {
     idOrDate: string
 }
 interface GetExecutionResp {
-  error: Common.Error
-  execution_and_activity: Common.ExecutionAndActivity
+     error: Common.Error
+     execution_and_activity: Common.ExecutionAndActivity
 }
 }
