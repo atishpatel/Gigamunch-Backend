c65ea70e42b43373d01c92078e653ee6e8018de0
Chris <chrissipe12@gmail.com>
2020-02-18 12:25:08 -0600

    made dinners page a bit prettier

--- subserver/web/src/App.vue
+++ subserver/web/src/App.vue
@@ -14,7 +14,7 @@
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>
-              My Dinners
+              Upcoming Dinners
             </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
